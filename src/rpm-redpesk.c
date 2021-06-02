5d051824924f402515a4b141ec45616de7bcb6dc
Clément Bénier <clement.benier@iot.bzh>
2021-06-02 19:07:36 +0200

    rpm-redpesk: rework install/remove/upgrading rpms
    - upgrading:
        - the first psm gives a tetype to TR_REMOVED then TR_ADDED and
          finally TR_REMOVED
        - uninstall need to be before the rpm install (pre_psm TR_ADDED hook)
        - install need to be after the rpm erase (TR_REMOVED post_psm hook)
    - install is a particular case of upgrading without uninstall
        - no TR_REMOVED in the first psm (no need to wait for rpm erase)
    - remove is a particular case of upgrading without install

--- src/rpm-redpesk.c
+++ src/rpm-redpesk.c
@@ -18,9 +18,73 @@
 #include "utils-systemd.h"
 #include "wgt-info.h"
 
+/***************************************************
+  sequence of INSTALL
+ 
+	tsm_pre(CHECK)
+	tsm_post(CHECK)
+	tsm_pre(APPLY)
+ 		psm_pre(TR_ADDED)   + files   -> [toinstall,!installed,!toremove,installdir]
+		psm_post(TR_ADDED)  + files   -> [toinstall,!installed,!toremove,installdir]
+			- install -
+		psm_pre(TR_ADDED)             -> [!toinstall,installed,!toremove,installdir]
+		psm_post(TR_ADDED)            -> [!toinstall,installed,!toremove,installdir]
+	tsm_post(APPLY)
+ 
+ sequence of REMOVE
+
+	tsm_pre(CHECK)
+	tsm_post(CHECK)
+	tsm_pre(APPLY)
+ 		psm_pre(TR_REMOVED)           -> [!toinstall,!installed,toremove,!installdir]
+		psm_post(TR_REMOVED)          -> [!toinstall,!installed,toremove,!installdir]
+		psm_pre(TR_REMOVED)  + files  -> [!toinstall,!installed,toremove,installdir]
+			- remove -
+		psm_post(TR_REMOVED) + files  -> [!toinstall,!installed,toremove,installdir]
+	tsm_post(APPLY)
+ 
+ sequence of UPGRADE
+
+	tsm_pre(CHECK)
+	tsm_post(CHECK)
+	tsm_pre(APPLY)
+ 		psm_pre(TR_REMOVED)           -> [!toinstall,!installed,toremove,!installdir]
+		psm_post(TR_REMOVED)          -> [!toinstall,!installed,toremove,!installdir]
+		psm_pre(TR_ADDED)    + files  -> [toinstall,!installed,toremove,installdir]
+			- remove -
+		psm_post(TR_ADDED)   + files  -> [toinstall,!installed,!toremove,installdir]
+		psm_pre(TR_REMOVED)  + files  -> [toinstall,!installed,!toremove,installdir]
+		psm_post(TR_REMOVED) + files  -> [toinstall,installed,!toremove,installdir]
+			- install -
+		psm_pre(TR_ADDED)             -> [toinstall,installed,!toremove,installdir]
+		psm_post(TR_ADDED)            -> [toinstall,installed,!toremove,installdir]
+	tsm_post(APPLY)
+
+ */
 static const char *afm_system_daemon = "afm-system-daemon";
 
-static int reinstall = 0;
+struct redpesk_pkgs {
+	char *name; //package name
+	char *installdir; //path to install directory
+	int toinstall;
+	int installed;
+	int toremove;
+	struct redpesk_pkgs *next;
+};
+
+/* packages list */
+static struct redpesk_pkgs *rpkgs_list = NULL;
+
+/* debug te type */
+static const char * typestring(rpmte te)
+{
+	switch (rpmteType(te)) {
+		case TR_ADDED: return "RPMTYPE TR_ADDED";
+		case TR_REMOVED: return "RPMTYPE TR_REMOVED";
+		case TR_RPMDB: return "RPMTYPE TR_RPMDB";
+		default: return "RPMTYPE UNKNOWN";
+	}
+}
 
 static int sighup_afm_main()
 {
@@ -56,6 +120,7 @@ static int install(const char *dirname)
 {
 	struct wgt_info *ifo;
 
+	rpmlog(RPMLOG_DEBUG, "[REDPESK]: install %s\n", dirname);
 	ifo = install_redpesk(dirname);
 	if (!ifo) {
 		rpmlog(RPMLOG_ERR, "Fail to install %s\n", dirname);
@@ -76,6 +141,7 @@ static int uninstall(const char *dirname)
 {
 	int rc;
 	/* uninstall the widget */
+	rpmlog(RPMLOG_DEBUG, "[REDPESK]: uninstall %s\n", dirname);
 	rc = uninstall_redpesk(dirname);
 	if(rc)
 		rpmlog(RPMLOG_ERR, "Fail to uninstall %s\n", dirname);
@@ -84,79 +150,147 @@ static int uninstall(const char *dirname)
 	return rc;
 }
 
-static char *lookingForConfigXml(rpmte te)
+static int getInstallDir(rpmte te, char **ptrDir)
 {
-	char *dir = NULL;
+	static const char configxml[] = "/config.xml";
+	int result = 0;
 	rpmfiles files = rpmteFiles(te);
 	rpmfi fi = rpmfilesIter(files, RPMFI_ITER_FWD);
 
-	while (rpmfiNext(fi) >= 0) {
+	*ptrDir = NULL;
+	while (result == 0 && rpmfiNext(fi) >= 0) {
 		const char *filepath = rpmfiFN(fi);
-
-		if (!strcmp("config.xml", basename((char *)filepath))) {
-			dir = strdup(dirname((char *)filepath));
-			rpmlog(RPMLOG_INFO, "config.xml found on %s\n", dir);
-			break;
+		size_t length = strlen(filepath);
+		if (length >= sizeof(configxml) - 1 /* assumes absolute path */
+		  && !strcmp(configxml, &filepath[length - sizeof(configxml) + 1])) {
+			*ptrDir = strndup(filepath, length - sizeof(configxml) + 1);
+			result = *ptrDir != NULL ? 1 : -1;
 		}
 	}
 
 	rpmfiFree(fi);
 	rpmfilesFree(files);
-	return dir;
+	return result;
+}
+
+static struct redpesk_pkgs *get_node_from_list(const char* name)
+{
+	if(!rpkgs_list)
+		return NULL;
+
+	struct redpesk_pkgs *node = rpkgs_list;
+	while (node != NULL) {
+		if (!strcmp(node->name, name))
+			return node;
+		node = node->next;
+	}
+	return NULL;
 }
 
 static rpmRC redpesk_psm_pre(rpmPlugin plugin, rpmte te)
 {
 	int rc;
+	const char *name = rpmteN(te);
+	char *dirname;
 
-	/* if transaction is not REMOVED, nothing to do */
-	if (rpmteType(te) != TR_REMOVED || reinstall)
-		return RPMRC_OK;
+	rpmlog(RPMLOG_DEBUG, "[REDPESK]: redpesk_psm_pre pkg=%s type=%s\n", name, typestring(te));
 
-	char *dirname = lookingForConfigXml(te);
+	struct redpesk_pkgs * pkg = get_node_from_list(name);
+	if(!pkg) {
+		/* add element pkg to list */
+		pkg = (struct redpesk_pkgs*)malloc(sizeof(struct redpesk_pkgs));
+		if(pkg == NULL) {
+			rpmlog(RPMLOG_ERR, "malloc failed for %s", name);
+			return RPMRC_FAIL;
+		}
+		pkg->name = strdup(name);
+		if(pkg->name == NULL) {
+			free(pkg);
+			rpmlog(RPMLOG_ERR, "malloc failed for %s", name);
+			return RPMRC_FAIL;
+		}
+		pkg->installdir = NULL;
+		pkg->toinstall = 0;
+		pkg->installed = 0;
+		pkg->toremove = rpmteType(te) == TR_REMOVED; //initialised with TR_REMOVED means upgrading if there will be an install
+		pkg->next = rpkgs_list;
+		rpkgs_list = pkg;
+	}
 
-	if (!dirname)
-		return RPMRC_OK;
+	/* get install directory */
+	dirname = pkg->installdir;
+	if (dirname == NULL) {
+		rc = getInstallDir(te, &dirname);
+		if (rc == 0)
+			return RPMRC_OK;
+		if (rc < 0) {
+			rpmlog(RPMLOG_ERR, "malloc failed for %s", name);
+			return RPMRC_FAIL;
+		}
+		pkg->installdir = dirname;
+	}
 
-	rc = uninstall(dirname);
-	free(dirname);
-	if(rc)
-		rpmlog(RPMLOG_WARNING, "issue uninstall lsm context: carry on removing package anyway\n");
+	/* need to redpesk_install at the end */
+	if(rpmteType(te) == TR_ADDED)
+		pkg->toinstall = 1;
+
+	/* uninstall */
+	if(pkg->toremove) {
+		rpmlog(RPMLOG_DEBUG, "[REDPESK]: uninstall %s\n", dirname);
+		rc = uninstall(dirname);
+		if(rc)
+			rpmlog(RPMLOG_WARNING, "issue uninstall lsm context: carry on removing package anyway\n");
+	}
 
 	return RPMRC_OK;
 }
 
 static rpmRC redpesk_psm_post(rpmPlugin plugin, rpmte te, int res)
 {
-	/* if transaction fail no need to install redpesk */
-	if (res != RPMRC_OK)
+	rpmRC ret = RPMRC_OK;
+	const char *name = rpmteN(te);
+
+	rpmlog(RPMLOG_DEBUG, "[REDPESK]: redpesk_psm_post pkg=%s type=%s\n", name, typestring(te));
+
+	struct redpesk_pkgs * pkg = get_node_from_list(name);
+	if(!pkg) {
+		rpmlog(RPMLOG_DEBUG, "No pkg in list for %s\n", name);
 		return RPMRC_OK;
-
-	/* if transaction is not install, nothing to do */
-	if (rpmteType(te) != TR_ADDED)
-		return RPMRC_OK;
-
-	/* looking for config.xml directory */
-	char *dirname = lookingForConfigXml(te);
-
-	/* if not found, nothing to do */
-	if (!dirname)
-		return RPMRC_OK;
-
-	/* needed if it is a reinstall process, in this case do not uninstall redpesk*/
-	reinstall = 1;
-
-	/* try to install redpesk */
-	if (install(dirname)) {
-		free(dirname);
-		return RPMRC_FAIL;
 	}
 
-	free(dirname);
+	/* if toremove and need to install: install */
+	if(!pkg->toremove && !pkg->installed && pkg->toinstall) {
+		/* try to install redpesk */
+		rpmlog(RPMLOG_DEBUG, "[REDPESK]: install %s\n", pkg->installdir);
+		pkg->installed = 1;
+		if (install(pkg->installdir))
+			ret = RPMRC_FAIL;
+	}
+
+	/* Clearing toremove is needed on upgrade to allow install in later TR_REMOVED */
+	if(rpmteType(te) == TR_ADDED)
+		pkg->toremove = 0;
+
+	return ret;
+}
+
+static rpmRC redpesk_tsm_post(rpmPlugin plugin, rpmts ts, int res)
+{
+	rpmlog(RPMLOG_DEBUG, "[REDPESK]: redpesk_tsm_post\n");
+
+	/* free list */
+	while(rpkgs_list != NULL) {
+		struct redpesk_pkgs *node = rpkgs_list;
+		rpkgs_list = node->next;
+		free(node->name);
+		free(node->installdir);
+		free(node);
+	}
 	return RPMRC_OK;
 }
 
 struct rpmPluginHooks_s redpesk_hooks = {
+	.tsm_post = redpesk_tsm_post,
 	.psm_pre = redpesk_psm_pre,
 	.psm_post = redpesk_psm_post,
 };
