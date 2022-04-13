486ff4a896979f17f53d11364b63676c87c57674
Jose Bollo <jose.bollo@iot.bzh>
2022-04-13 14:28:34 +0200

    path-entry: Introduce path-entry

---
+++ src/path-entry.c
@@ -0,0 +1,491 @@
+/*
+ Copyright (C) 2015-2022 IoT.bzh Company
+
+ Author: José Bollo <jose.bollo@iot.bzh>
+
+ $RP_BEGIN_LICENSE$
+ Commercial License Usage
+  Licensees holding valid commercial IoT.bzh licenses may use this file in
+  accordance with the commercial license agreement provided with the
+  Software or, alternatively, in accordance with the terms contained in
+  a written agreement between you and The IoT.bzh Company. For licensing terms
+  and conditions see https://www.iot.bzh/terms-conditions. For further
+  information use the contact form at https://www.iot.bzh/contact.
+
+ GNU General Public License Usage
+  Alternatively, this file may be used under the terms of the GNU General
+  Public license version 3. This license is as published by the Free Software
+  Foundation and appearing in the file LICENSE.GPLv3 included in the packaging
+  of this file. Please review the following information to ensure the GNU
+  General Public License requirements will be met
+  https://www.gnu.org/licenses/gpl-3.0.html.
+ $RP_END_LICENSE$
+*/
+
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <limits.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "path-entry.h"
+
+#define FLAGS_IS_ADDED 1
+
+typedef struct var
+{
+	const void *key;
+	struct var *next;
+	void *value;
+	void (*dispose)(void*);
+}
+	var_t;
+
+struct path_entry {
+	path_entry_t *parent;
+	path_entry_t *children;
+	path_entry_t *sibling;
+	var_t *vars;
+	unsigned length;
+	unsigned char flags;
+	char name[];
+};
+
+static path_entry_t *search_child(path_entry_t *parent, const char *name, unsigned length)
+{
+	path_entry_t *entry = parent->children;
+	while (entry != NULL && (entry->length != length || memcmp(name, entry->name, length)))
+		entry = entry->sibling;
+	return entry;
+}
+
+static path_entry_t *make_child(path_entry_t *parent, const char *name, unsigned length)
+{
+	path_entry_t **prv, *entry = malloc(length + 1 + sizeof *entry);
+
+	if (entry != NULL) {
+		entry->parent = parent;
+		entry->children = NULL;
+		entry->vars = NULL;
+		entry->length = length;
+		entry->flags = 0;
+		entry->name[length] = 0;
+		memcpy(entry->name, name, length);
+		prv = &parent->children;
+		while (*prv != NULL && strcmp((*prv)->name, name) < 0)
+			prv = &(*prv)->sibling;
+		entry->sibling = *prv;
+		*prv = entry;
+	}
+	return entry;
+}
+
+static path_entry_t *get_child(path_entry_t *parent, const char *name, unsigned length)
+{
+	return search_child(parent, name, length) ?: make_child(parent, name, length);
+}
+
+static path_entry_t *process(
+		path_entry_t *root,
+		path_entry_t *(*get)(path_entry_t*, const char*, unsigned),
+		const char *path,
+		unsigned length
+) {
+	unsigned end, beg;
+	path_entry_t *entry = root;
+
+	for (beg = 0 ; beg < length && path[beg] == '/' ; beg++);
+	while (entry != NULL && beg < length) {
+		for (end = beg ; end < length && path[end] != '/' ; end++);
+		entry = get(entry, &path[beg], end - beg);
+		for (beg = end ; beg < length && path[beg] == '/' ; beg++);
+	}
+	return entry;
+}
+
+int path_entry_is_root(path_entry_t *entry)
+{
+	return entry->parent == NULL;
+}
+
+int path_entry_create_root(path_entry_t **root)
+{
+	path_entry_t *entry = malloc(1 + sizeof *entry);
+	*root = entry;
+	if (entry == NULL)
+		return -ENOMEM;
+
+	entry->parent = NULL;
+	entry->children = NULL;
+	entry->sibling = NULL;
+	entry->vars = NULL;
+	entry->length = 0;
+	entry->flags = 0;
+	entry->name[0] = 0;
+	return 0;
+}
+
+static void destroy(path_entry_t *entry)
+{
+	var_t *var;
+	path_entry_t *child = entry->children;
+	while (child != NULL) {
+		path_entry_t *sibling = child->sibling;
+		destroy(child);
+		child = sibling;
+	}
+	var = entry->vars;
+	while(var != NULL) {
+		var_t *next = var->next;
+		free(var);
+		var = next;
+	}
+	free(entry);
+}
+
+void path_entry_destroy(path_entry_t *entry)
+{
+	path_entry_t *other;
+	if (entry != NULL) {
+		/* unlink from parents if needed */
+		other = entry->parent;
+		if (other != NULL) {
+			if (other->children == entry)
+				other->children = entry->sibling;
+			else {
+				other = other->children;
+				while (other->sibling != entry)
+					other = other->sibling;
+				other->sibling = entry->sibling;
+			}
+		}
+		/* full destroy */
+		destroy(entry);
+	}
+}
+
+int path_entry_add_length(path_entry_t *root, path_entry_t **result, const char *path, size_t length)
+{
+	path_entry_t *entry = process(root, get_child, path, (unsigned)length);
+	if (result != NULL)
+		*result = entry;
+	if (entry == NULL)
+		return -ENOMEM;
+	entry->flags |= FLAGS_IS_ADDED;
+	return 0;
+}
+
+int path_entry_add(path_entry_t *root, path_entry_t **result, const char *path)
+{
+	return path_entry_add_length(root, result, path, path == NULL ? 0 : strlen(path));
+}
+
+int path_entry_get_length(const path_entry_t *root, path_entry_t **result, const char *path, size_t length)
+{
+	path_entry_t *entry = process((path_entry_t*)root, search_child, path, (unsigned)length);
+	if (result != NULL)
+		*result = entry;
+	return entry == NULL ? -ENOENT : 0;
+}
+
+int path_entry_get(const path_entry_t *root, path_entry_t **result, const char *path)
+{
+	return path_entry_get_length(root, result, path, path == NULL ? 0 : strlen(path));
+}
+
+int path_entry_root_prepend_length(path_entry_t *root, path_entry_t **entry, const char *path, size_t length)
+{
+	path_entry_t *children, *tail;
+	int rc;
+
+	if (root->parent != NULL) {
+		tail = NULL;
+		rc = -EINVAL;
+	}
+	else {
+		children = root->children;
+		root->children = NULL;
+		tail = process(root, get_child, path, (unsigned)length);
+		if (tail == NULL) {
+			root->children = children;
+			rc = -ENOMEM;
+		}
+		else {
+			tail->children = children;
+			while (children != NULL) {
+				children->parent = tail;
+				children = children->sibling;
+			}
+			rc = 0;
+		}
+	}
+	if (entry)
+		*entry = tail;
+	return rc;
+}
+
+int path_entry_root_prepend(path_entry_t *root, path_entry_t **entry, const char *path)
+{
+	return path_entry_root_prepend_length(root, entry, path, strlen(path));
+}
+
+int path_entry_was_added(const path_entry_t *entry)
+{
+	return (entry->flags & FLAGS_IS_ADDED) != 0;
+}
+
+const char *path_entry_name(const path_entry_t *entry)
+{
+	return entry->name;
+}
+
+size_t path_entry_length(const path_entry_t *entry)
+{
+	return entry->length;
+}
+
+path_entry_t *path_entry_parent(const path_entry_t *entry)
+{
+	return entry->parent;
+}
+
+int path_entry_has_child(const path_entry_t *entry)
+{
+	return entry->children != NULL;
+}
+
+size_t path_entry_get_relpath(const path_entry_t *entry, char *buffer, size_t length, const path_entry_t *root)
+{
+	size_t result, offset;
+
+	if (entry == NULL || entry == root)
+		result = 0;
+	else {
+		offset = path_entry_get_relpath(entry->parent, buffer, length, root);
+		if (entry->length == 0)
+			result = offset;
+		else {
+			result = offset + entry->length + 1;
+			if (offset < length) {
+				buffer[offset++] = '/';
+				length -= offset;
+				if (length > 0) {
+					if (entry->length >= length)
+						length = length - 1;
+					else
+						length = entry->length;
+					memcpy(&buffer[offset], entry->name, length);
+					buffer[offset + length] = 0;
+				}
+			}
+		}
+	}
+	return result;
+}
+
+size_t path_entry_get_path(const path_entry_t *entry, char *buffer, size_t length)
+{
+	return path_entry_get_relpath(entry, buffer, length, NULL);
+}
+
+struct foreach {
+	void *closure;
+	int (*fun)(void *closure, path_entry_t *entry, const char *path, size_t length);
+	unsigned flags;
+	size_t length;
+	size_t size;
+	char *buffer;
+};
+
+static int do_foreach(struct foreach *fe, path_entry_t *entry)
+{
+	int rc = 0, emit;
+	size_t memo;
+	path_entry_t *child;
+
+	if (fe->flags & PATH_ENTRY_FORALL_SILENT_ROOT) {
+		fe->flags ^= PATH_ENTRY_FORALL_SILENT_ROOT;
+		emit = 0;
+	}
+	else {
+		if (entry->length > 0 && !(fe->flags & PATH_ENTRY_FORALL_NO_PATH)) {
+			if (fe->length + entry->length + 2 > fe->size)
+				return -ENAMETOOLONG;
+			fe->buffer[fe->length++] = '/';
+			memcpy(&fe->buffer[fe->length], entry->name, entry->length);
+			fe->length += entry->length;
+		}
+		emit = !(fe->flags & PATH_ENTRY_FORALL_ONLY_ADDED) || (entry->flags & FLAGS_IS_ADDED);
+	}
+	memo = fe->length;
+	if (emit && (fe->flags & PATH_ENTRY_FORALL_BEFORE)) {
+		if (!(fe->flags & PATH_ENTRY_FORALL_NO_PATH))
+			fe->buffer[memo] = 0;
+		rc = fe->fun(fe->closure, entry, fe->buffer, memo);
+	}
+	for (child = entry->children; rc == 0 && child != NULL ; child = child->sibling) {
+		fe->length = memo;
+		rc = do_foreach(fe, child);
+	}
+	if (emit && rc == 0 && (fe->flags & PATH_ENTRY_FORALL_AFTER)) {
+		if (!(fe->flags & PATH_ENTRY_FORALL_NO_PATH))
+			fe->buffer[memo] = 0;
+		rc = fe->fun(fe->closure, entry, fe->buffer, memo);
+	}
+	return rc;
+}
+
+int path_entry_for_each_in_buffer(
+	unsigned flags,
+	path_entry_t *root,
+	int (*fun)(void *closure, path_entry_t *entry, const char *path, size_t length),
+	void *closure,
+	char *buffer,
+	size_t size
+) {
+	struct foreach fe;
+	if ((flags & (PATH_ENTRY_FORALL_BEFORE|PATH_ENTRY_FORALL_AFTER)) == 0)
+		flags |= PATH_ENTRY_FORALL_AFTER;
+	fe.closure = closure;
+	fe.fun = fun;
+	fe.flags = flags;
+	if ((flags & (PATH_ENTRY_FORALL_ABSOLUTE|PATH_ENTRY_FORALL_NO_PATH)) != PATH_ENTRY_FORALL_ABSOLUTE)
+		fe.length = 0;
+	else
+		fe.length = path_entry_get_path(root, buffer, size);
+	fe.size = size;
+	fe.buffer = buffer;
+	return root == NULL ? 0 : do_foreach(&fe, root);
+}
+
+static int for_each_scratch(
+	unsigned flags,
+	path_entry_t *root,
+	int (*fun)(void *closure, path_entry_t *entry, const char *path, size_t length),
+	void *closure
+) {
+	char buffer[PATH_MAX];
+	return path_entry_for_each_in_buffer(flags, root, fun, closure, buffer, sizeof buffer);
+}
+
+int path_entry_for_each(
+	unsigned flags,
+	path_entry_t *root,
+	int (*fun)(void *closure, path_entry_t *entry, const char *path, size_t length),
+	void *closure
+) {
+	if (flags & PATH_ENTRY_FORALL_NO_PATH)
+		return path_entry_for_each_in_buffer(flags, root, fun, closure, NULL, 0);
+	return for_each_scratch(flags, root, fun, closure);
+}
+
+
+
+
+static var_t *search_var(var_t *var, const void *key)
+{
+	while(var != NULL && key != var->key)
+		var = var->next;
+	return var;
+}
+
+static void *varval(var_t *var)
+{
+	return var == NULL ? NULL : var->value;
+}
+
+int path_entry_var_exists(const path_entry_t *entry, const void *key)
+{
+	return search_var(entry->vars, key) != NULL;
+}
+
+void *path_entry_var(const path_entry_t *entry, const void *key)
+{
+	return varval(search_var(entry->vars, key));
+}
+
+int path_entry_var_set(path_entry_t *entry, const void *key, void *value, void (*dispose)(void*))
+{
+	var_t *var = search_var(entry->vars, key);
+	if (var == NULL) {
+		var = malloc(sizeof *var);
+		if (var == NULL)
+			return -ENOMEM;
+		var->key = key;
+		var->next = entry->vars;
+		entry->vars = var;
+	}
+	else if (var->dispose != NULL)
+		var->dispose(var->value);
+	var->value = value;
+	var->dispose = dispose;
+	return 0;
+}
+
+void path_entry_var_del(path_entry_t *entry, const void *key)
+{
+	var_t *var, **prv = &entry->vars;
+	while ((var = *prv) != NULL) {
+		if (var->key == key) {
+			*prv = var->next;
+			if (var->dispose != NULL)
+				var->dispose(var->value);
+			free(var);
+			break;
+		}
+		prv = &var->next;
+	}
+}
+
+unsigned path_entry_var_count(const path_entry_t *entry)
+{
+	var_t *var = entry->vars;
+	unsigned result = 0;
+	while(var != NULL) {
+		var = var->next;
+		result++;
+	}
+	return result;
+}
+
+void *path_entry_var_at(const path_entry_t *entry, unsigned index, const void **key)
+{
+	var_t *var = entry->vars;
+	unsigned idx = 0;
+	while(var != NULL && idx != index) {
+		var = var->next;
+		idx++;
+	}
+	if (key != NULL)
+		*key = var->key;
+	return varval(var);
+}
+
+int path_entry_add_from_file(path_entry_t *root, FILE *file)
+{
+	int rc = 0;
+	size_t idx;
+	char path[PATH_MAX];
+
+	while (rc == 0 && fgets(path, sizeof path, file) != NULL) {
+		size_t len = strlen(path);
+		while (len && isspace(path[len - 1]))
+			len--;
+		if (len && path[0] == '#') {
+			if (len == 6 && path[1] == 'S' && path[2] == 'T'
+			 && path[3] == 'O' && path[4] == 'P' && path[5] == '#')
+				break;
+		}
+		else {
+			for (idx = 0 ; idx < len && isspace(path[idx]) ; idx++);
+			if (idx < len)
+				rc = path_entry_add_length(root, NULL, &path[idx], len - idx);
+		}
+	}
+	if (rc == 0 && ferror(file))
+		rc = -errno;
+	return rc;
+}
---
+++ src/path-entry.h
@@ -0,0 +1,288 @@
+/*
+ Copyright (C) 2015-2022 IoT.bzh Company
+
+ Author: José Bollo <jose.bollo@iot.bzh>
+
+ $RP_BEGIN_LICENSE$
+ Commercial License Usage
+  Licensees holding valid commercial IoT.bzh licenses may use this file in
+  accordance with the commercial license agreement provided with the
+  Software or, alternatively, in accordance with the terms contained in
+  a written agreement between you and The IoT.bzh Company. For licensing terms
+  and conditions see https://www.iot.bzh/terms-conditions. For further
+  information use the contact form at https://www.iot.bzh/contact.
+
+ GNU General Public License Usage
+  Alternatively, this file may be used under the terms of the GNU General
+  Public license version 3. This license is as published by the Free Software
+  Foundation and appearing in the file LICENSE.GPLv3 included in the packaging
+  of this file. Please review the following information to ensure the GNU
+  General Public License requirements will be met
+  https://www.gnu.org/licenses/gpl-3.0.html.
+ $RP_END_LICENSE$
+*/
+
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * @brief abstract path entry
+ */
+typedef struct path_entry path_entry_t;
+
+/**
+ * @brief creates the root entry
+ *
+ * @param root the root
+ * @return 0 on success or -ENOMEM
+ */
+extern int path_entry_create_root(path_entry_t **root);
+
+/**
+ * @brief destroy the given entry and all its content
+ *
+ * @param entry the entry to be destroyed
+ */
+extern void path_entry_destroy(path_entry_t *entry);
+
+/**
+ * @brief get or add to the root the entry for the path whose length is given
+ *
+ * @param root the root
+ * @param result where to store the result (can be NULL)
+ * @param path the path from the root
+ * @param length the length of the path
+ * @return 0 on success or -ENOMEM
+ */
+extern int path_entry_add_length(path_entry_t *root, path_entry_t **result, const char *path, size_t length);
+
+/**
+ * @brief get or add to the root the entry for the path
+ *
+ * @param root the root
+ * @param result where to store the result (can be NULL)
+ * @param path the path from the root
+ * @return 0 on success or -ENOMEM
+ */
+extern int path_entry_add(path_entry_t *root, path_entry_t **result, const char *path);
+
+/**
+ * @brief get from the root the entry for the path whose length is given
+ *
+ * @param root the root
+ * @param result where to store the result (can be NULL)
+ * @param path the path from the root
+ * @param length the length of the path
+ * @return 0 on success or -ENOENT
+ */
+extern int path_entry_get_length(const path_entry_t *root, path_entry_t **result, const char *path, size_t length);
+
+/**
+ * @brief get from the root the entry for the path
+ *
+ * @param root the root
+ * @param result where to store the result (can be NULL)
+ * @param path the path from the root
+ * @return 0 on success or -ENOENT
+ */
+extern int path_entry_get(const path_entry_t *root, path_entry_t **result, const char *path);
+
+/**
+ * @brief prepend the path to the root, on succes, the old root is now at the given
+ * path and is returned in entry.
+ * 
+ * @param root the root to alter
+ * @param entry the entry corresponding to the previous root (if not NULL)
+ * @param path the path of the root
+ * @param length the length of the path
+ * @return 0 on success, -EINVAL if root is not a root, -ENOMEM on case of memory depletion
+ */
+extern int path_entry_root_prepend_length(path_entry_t *root, path_entry_t **entry, const char *path, size_t length);
+
+/**
+ * @brief prepend the path to the root, on succes, the old root is now at the given
+ * path and is returned in entry.
+ * 
+ * @param root the root to alter
+ * @param entry the entry corresponding to the previous root (if not NULL)
+ * @param path the path of the root
+ * @return 0 on success, -EINVAL if root is not a root, -ENOMEM on case of memory depletion
+ */
+extern int path_entry_root_prepend(path_entry_t *root, path_entry_t **entry, const char *path);
+
+/**
+ * check if the entry was added
+ *
+ * @param entry the entry to inspect
+ * @return a non zero value if it was added
+ */
+extern int path_entry_was_added(const path_entry_t *entry);
+
+/**
+ * get the name of the entry
+ *
+ * @param entry the entry to inspect
+ * @return the name of the entry
+ */
+extern const char *path_entry_name(const path_entry_t *entry);
+
+/**
+ * get the length of the name of the entry
+ *
+ * @param entry the entry to inspect
+ * @return the length of the name
+ */
+extern size_t path_entry_length(const path_entry_t *entry);
+
+/**
+ * get the parent of the entry
+ *
+ * @param entry the entry to inspect
+ * @return the parent of the entry or NULL if none
+ */
+extern path_entry_t *path_entry_parent(const path_entry_t *entry);
+
+/**
+ * check if the entry has child
+ *
+ * @param entry the entry to inspect
+ * @return zero if not child, otherwise 1 if children
+ */
+extern int path_entry_has_child(const path_entry_t *entry);
+
+/**
+ * @brief get in buffer the path of the given entry
+ *
+ * @param entry the entry to get
+ * @param buffer the buffer for storing path (can be NULL when length == 0)
+ * @param length the length of the buffer
+ * @return the length of the path (excluding trailing nul) as if length was enough
+ */
+extern size_t path_entry_get_path(const path_entry_t *entry, char *buffer, size_t length);
+
+/**
+ * @brief get in buffer the relative path of the given entry from root
+ *
+ * @param entry the entry to get
+ * @param buffer the buffer for storing path (can be NULL when length == 0)
+ * @param length the length of the buffer
+ * @param root the relative root, can be NULL
+ * @return the length of the path (excluding trailing nul) as if length was enough
+ */
+extern size_t path_entry_get_relpath(const path_entry_t *entry, char *buffer, size_t length, const path_entry_t *root);
+
+#define PATH_ENTRY_FORALL_ONLY_ADDED         1
+#define PATH_ENTRY_FORALL_NO_PATH            2
+#define PATH_ENTRY_FORALL_BEFORE             4
+#define PATH_ENTRY_FORALL_AFTER              8
+#define PATH_ENTRY_FORALL_ABSOLUTE          16
+#define PATH_ENTRY_FORALL_SILENT_ROOT       32
+
+/**
+ * @brief iterate over entries until function returns a not zero value
+ *
+ * @param flags for to control iteration (see PATH_ENTRY_FORALL_...)
+ * @param root root of the entries
+ * @param fun function to call for each entry
+ * @param closure closure for the function
+ * @return 0 on success or -ENAMETOOLONG if cancelled
+ */
+extern
+int path_entry_for_each(
+	unsigned flags,
+	path_entry_t *root,
+	int (*fun)(void *closure, path_entry_t *entry, const char *path, size_t length),
+	void *closure
+);
+
+/**
+ * @brief iterate over all entries with a buffer
+ *
+ * @param flags for to control iteration (see PATH_ENTRY_FORALL_...)
+ * @param root root of the entries
+ * @param fun function to call for each entry
+ * @param closure closure for the function
+ * @param buffer the buffer to use for storing paths
+ * @param size the size of the buffer
+ * @return 0 on success or -ENAMETOOLONG if cancelled
+ */
+extern
+int path_entry_for_each_in_buffer(
+	unsigned flags,
+	path_entry_t *root,
+	int (*fun)(void *closure, path_entry_t *entry, const char *path, size_t length),
+	void *closure,
+	char *buffer,
+	size_t size
+);
+
+/**
+ * @brief check if a var exist for the entry
+ *
+ * @param entry the entry to check
+ * @param key the variable key
+ *
+ * @return 1 if the var exists or 0 otherwise
+ */
+extern int path_entry_var_exists(const path_entry_t *entry, const void *key);
+
+/**
+ * @brief get the value of the var of the entry
+ *
+ * @param entry the entry
+ * @param key the key of the variable
+ *
+ * @return the value of the variable
+ */
+extern void *path_entry_var(const path_entry_t *entry, const void *key);
+
+/**
+ * @brief set the value of a variable
+ * 
+ * @param entry the entry
+ * @param key the key of the variable
+ * @param value the value to set
+ * @param dispose a function to call when variable is deleted
+ *
+ * @return 0 on success or -ENOMEM
+ */
+extern int path_entry_var_set(path_entry_t *entry, const void *key, void *value, void (*dispose)(void*));
+
+/**
+ * @brief delete the variable of key of the entry
+ * 
+ * @param entry the entry
+ * @param key the key of the variable to delete
+ */
+extern void path_entry_var_del(path_entry_t *entry, const void *key);
+
+/**
+ * @brief get the count of existing variables
+ * 
+ * @param entry the entry
+ *
+ * @return the count of existing variables
+ */
+extern unsigned path_entry_var_count(const path_entry_t *entry);
+
+/**
+ * @brief get a variable by its index
+ * 
+ * @param entry the entry
+ * @param index the index of the variable
+ * @param key pointer receiving the key if not NULL
+ *
+ * @return the value of the variable
+ */
+extern void *path_entry_var_at(const path_entry_t *entry, unsigned index, const void **key);
+
+/**
+ * @brief read the file and add its entry to the root
+ * 
+ * @param root the root to fulfill
+ * @param file the file to read
+ * @return 0 on success or a negative error code
+ */
+extern int path_entry_add_from_file(path_entry_t *root, FILE *file);
