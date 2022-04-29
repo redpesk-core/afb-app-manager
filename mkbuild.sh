38450a5bee3a585c0393a8ab9dde65fbca7eb5af
José Bollo <jose.bollo@iot.bzh>
2022-04-29 11:13:09 +0200

    Add a mkbuild script

---
+++ mkbuild.sh
@@ -0,0 +1,60 @@
+#/bin/sh
+
+h="$(dirname $0)"
+force=false
+: ${bd:=build}
+: ${PREFIX:=$HOME/.local}
+eval set -- $(getopt -o b:fp: -l buildir:,force,prefix: -- "$@") || exit
+while :; do
+	case "$1" in
+	-b|--buildir) bd="$2"; shift;;
+	-f|--force) force=true;;
+	-p|--prefix) PREFIX="$2"; shift;;
+	--) shift; break;;
+	esac
+	shift
+done
+
+cd "$h"
+h=$(pwd)
+mkdir -p "$bd" || exit
+cd "$bd" || exit
+
+$force && { rm -r * 2>/dev/null || rm CMakeCache.txt 2>/dev/null; }
+test -f CMakeCache.txt -a -f Makefile || \
+cmake \
+	-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX:=$PREFIX} \
+	-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Debug} \
+	-DDISTINCT_VERSIONS=${DISTINCT_VERSIONS:=OFF} \
+	-DUSE_LIBZIP=${USE_LIBZIP:=ON} \
+	-DUSE_SIMULATION=${USE_SIMULATION:=OFF} \
+	-DUSE_SDK=${USE_SDK:=OFF} \
+	-DALLOW_NO_SIGNATURE=${ALLOW_NO_SIGNATURE:=OFF} \
+	-DINSTALL_SAMPLE_KEYS=${INSTALL_SAMPLE_KEYS:=OFF} \
+	-DWITH_RPM_REDPESK_PLUGIN=${WITH_RPM_REDPESK_PLUGIN:=ON} \
+	-DWITH_TOOLS=${WITH_TOOLS:=ON} \
+	-DALLOW_WIDGETS=${ALLOW_WIDGETS:=OFF} \
+	-DWITH_OPENSSL=${WITH_OPENSSL:=OFF} \
+	-DSIMULATE_SECMGR=${SIMULATE_SECMGR:=OFF} \
+	${afm_name+-Dafm_name="${afm_name}"} \
+	${afm_confdir+-Dafm_confdir="${afm_confdir}"} \
+	${afm_datadir+-Dafm_datadir="${afm_datadir}"} \
+	${afm_libexecdir+-Dafm_libexecdir="${afm_libexecdir}"} \
+	${afm_appdir+-Dafm_appdir="${afm_appdir}"} \
+	${afm_icondir+-Dafm_icondir="${afm_icondir}"} \
+	${afm_prefix+-Dafm_prefix="${afm_prefix}"} \
+	${afm_units_root+-Dafm_units_root="${afm_units_root}"} \
+	${wgtpkg_trusted_certs_dir+-Dwgtpkg_trusted_certs_dir="${wgtpkg_trusted_certs_dir}"} \
+	${wgtpkg_sample_keys_dir+-Dwgtpkg_sample_keys_dir="${wgtpkg_sample_keys_dir}"} \
+	${wgtpkg_sample_certs_dir+-Dwgtpkg_sample_certs_dir="${wgtpkg_sample_certs_dir}"} \
+	${afm_platform_rundir+-Dafm_platform_rundir="${afm_platform_rundir}"} \
+	${afm_users_rundir+-Dafm_users_rundir="${afm_users_rundir}"} \
+	${afm_scope_platform_dir+-Dafm_scope_platform_dir="${afm_scope_platform_dir}"} \
+	${rpm_plugin_dir+-Drpm_plugin_dir="${rpm_plugin_dir}"} \
+	${AFMPKG_SOCKET_ADDRESS+-DAFMPKG_SOCKET_ADDRESS="${AFMPKG_SOCKET_ADDRESS}"} \
+	${SYSCONFDIR_DBUS_SYSTEM+-DSYSCONFDIR_DBUS_SYSTEM="${SYSCONFDIR_DBUS_SYSTEM}"} \
+	${SYSCONFDIR_PAMD+-DSYSCONFDIR_PAMD="${SYSCONFDIR_PAMD}"} \
+	${UNITDIR_SYSTEM+-DUNITDIR_SYSTEM="${UNITDIR_SYSTEM}"} \
+	"$h"
+
+make -j "$@"
