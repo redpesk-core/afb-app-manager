redpesk-core/afb-app-manager

commit fe20097355055a2d28bc6a78c34a938f689ca8a0

Fix case of id for migration

src/wgtpkg/wgt-json.c
@@ -510,7 +510,8 @@ static struct json_object *to_json(const struct wgt_desc *desc)
 
 	/* fills the main */
 	rc2 = j_add_many_strings_m(result,
-		string_id, desc->id,
+		//string_id, desc->id,
+		string_id_lower, desc->id_lower,
 		string_id_underscore, desc->id_underscore,
 		string_idaver, desc->idaver,
 		string_version, desc->version,

src/wgtpkg/wgt-info.c
@@ -172,8 +172,9 @@ static int fill_desc(struct wgt_desc *desc, int want_icons, int want_features, i
 		errno = EINVAL;
 		return -1;
 	}
-	make_lowercase(desc->id);
-	desc->id_underscore = strdup(desc->id);
+	desc->id_lower = strdup(desc->id);
+	make_lowercase(desc->id_lower);
+	desc->id_underscore = strdup(desc->id_lower);
 	dash_to_underscore(desc->id_underscore);
 
 	desc->version = (char*)xmlGetProp(node, (const xmlChar*)string_version);
@@ -184,7 +185,7 @@ static int fill_desc(struct wgt_desc *desc, int want_icons, int want_features, i
 	}
 	desc->ver = mkver(desc->version);
 	make_lowercase(desc->ver);
-	desc->idaver = mkidaver(desc->id, desc->ver);
+	desc->idaver = mkidaver(desc->id_lower, desc->ver);
 	desc->width = getpropnum(node, string_width, 0);
 	desc->height = getpropnum(node, string_height, 0);
 	desc->viewmodes = (char*)xmlGetProp(node, (const xmlChar*)string_viewmodes);
@@ -330,6 +331,7 @@ static void free_desc(struct wgt_desc *desc)
 	struct wgt_desc_param *param;
 
 	xmlFree(desc->id);
+	free(desc->id_lower);
 	free(desc->id_underscore);
 	xmlFree(desc->version);
 	free(desc->ver);
@@ -386,6 +388,7 @@ static void dump_desc(struct wgt_desc *desc, FILE *f, const char *prefix)
 	struct wgt_desc_param *param;
 
 	if (desc->id) fprintf(f, "%sid: %s\n", prefix, desc->id);
+	if (desc->id_lower) fprintf(f, "%sid-lower: %s\n", prefix, desc->id_lower);
 	if (desc->id_underscore) fprintf(f, "%sid-underscore: %s\n", prefix, desc->id_underscore);
 	if (desc->version) fprintf(f, "%sversion: %s\n", prefix, desc->version);
 	if (desc->ver) fprintf(f, "%sver: %s\n", prefix, desc->ver);

src/wgtpkg/wgt-info.h
@@ -53,6 +53,7 @@ struct wgt_desc_preference {
 struct wgt_desc {
 	int refcount;
 	char *id;
+	char *id_lower;
 	char *id_underscore;
 	char *version;
 	char *ver;

src/wgtpkg/wgt-strings.c
@@ -34,6 +34,7 @@ const char string_height[] = "height";
 const char string_href[] = "href";
 const char string_icon[] = "icon";
 const char string_id[] = "id";
+const char string_id_lower[] = "id-lower";
 const char string_id_underscore[] = "id-underscore";
 const char string_license[] = "license";
 const char string_name[] = "name";

src/wgtpkg/wgt-strings.h
@@ -34,6 +34,7 @@ extern const char string_height[];
 extern const char string_href[];
 extern const char string_icon[];
 extern const char string_id[];
+extern const char string_id_lower[];
 extern const char string_id_underscore[];
 extern const char string_license[];
 extern const char string_name[];

src/wgtpkg/wgtpkg-install.c
@@ -109,7 +109,7 @@ static int check_temporary_constraints(const struct wgt_desc *desc)
 {
 	int result;
 
-	result  = check_valid_string(desc->id, "id");
+	result  = check_valid_string(desc->id_lower, "id");
 	result |= check_valid_string(desc->version, "version");
 	result |= check_valid_string(desc->ver, "ver");
 	result |= check_defined(desc->content_src, "content");
@@ -446,7 +446,7 @@ static int install_security(const struct wgt_desc *desc)
 	struct pathent *pe0, *pe2, *ppe;
 
 	pe0 = NULL;
-	rc = secmgr_begin(desc->id);
+	rc = secmgr_begin(desc->id_lower);
 	if (rc < 0)
 		goto end;
 
@@ -699,9 +699,9 @@ static int get_target_directory(char target[PATH_MAX], const char *root, const s
 	int rc;
 
 #if DISTINCT_VERSIONS
-	rc = snprintf(target, PATH_MAX, "%s/%s/%s", root, desc->id, desc->ver);
+	rc = snprintf(target, PATH_MAX, "%s/%s/%s", root, desc->id_lower, desc->ver);
 #else
-	rc = snprintf(target, PATH_MAX, "%s/%s", root, desc->id);
+	rc = snprintf(target, PATH_MAX, "%s/%s", root, desc->id_lower);
 #endif
 	if (rc < PATH_MAX)
 		rc = 0;

src/wgtpkg/wgtpkg-uninstall.c
@@ -71,7 +71,7 @@ static int setdown_files_and_security(const struct wgt_desc *desc)
 	}
 
 #if DISTINCT_VERSIONS
-	rc = secmgr_begin(desc->id);
+	rc = secmgr_begin(desc->id_lower);
 #else
 	rc = secmgr_begin(desc->idaver);
 #endif

commit d4015e2aca48aa59bcf0923d94f8b4560a117847

Fix installation of widgets with config.xml
The commit fe20097355055a2d28bc6a78c34a938f689ca8a0
introduced a regression because after it id was no more
in the meta data used by the installer.

src/wgtpkg/wgt-json.c
@@ -510,7 +510,7 @@ static struct json_object *to_json(const struct wgt_desc *desc)
 
 	/* fills the main */
 	rc2 = j_add_many_strings_m(result,
-		//string_id, desc->id,
+		string_id, desc->id_lower,
 		string_id_lower, desc->id_lower,
 		string_id_underscore, desc->id_underscore,
 		string_idaver, desc->idaver,